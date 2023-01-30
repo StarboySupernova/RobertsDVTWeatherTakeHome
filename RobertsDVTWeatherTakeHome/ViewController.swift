24f7aab242047a05886a351e86e2114519c3623f
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2023-01-31 00:57:21 +0100

    Checkpoint 31.01.23 02h00

--- RobertsDVTWeatherTakeHome/ViewController.swift
+++ RobertsDVTWeatherTakeHome/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var heroCardView: UIView!
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        heroCardView.layer.cornerCurve = .continuous
+        heroCardView.layer.cornerRadius = 30
+        blurView.layer.cornerCurve = .continuous
+        blurView.layer.cornerRadius = 30
+        blurView.layer.masksToBounds = true // Without this line, we are not able to see the corner radius
+        
+        heroCardView.layer.shadowColor = UIColor(named: "Shadow")!.cgColor
+        heroCardView.layer.shadowOpacity = 0.5
+        heroCardView.layer.shadowOffset = CGSize(width: 0, height: 10)
+        heroCardView.layer.shadowRadius = 20
     }
-
-
+    
+    
 }
 
